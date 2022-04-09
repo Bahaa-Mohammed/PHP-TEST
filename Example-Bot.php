a1d3122053a34eb5bbb804e932d08138ecce0262
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-04-09 09:37:05 +0300

    Update Example-Bot.php

--- Example-Bot.php
+++ Example-Bot.php
@@ -18,6 +18,6 @@ $firstname	= $bot->getChatFirstname();
 $message_id	= $bot->MessageId();
 
 if($text == "/start"){
-    $r = $bot->sendMessage($chat_id, "<b>Hello</b> <a href='tg://user?id=$chat_id'>$firstname</a> !\n\n<b>Special Thanks for using iNeoTeam Telegram Bot Class.</b>\n\n<b>GitHub:</b> https://github.com/iNeoTeam/iTelegram\n<b>Powered By</b> @iNeoTeam.", "HTML", true);
+    $bot->sendMessage($chat_id, "<b>Hello</b> <a href='tg://user?id=$chat_id'>$firstname</a> !\n\n<b>Special Thanks for using iNeoTeam Telegram Bot Class.</b>\n\n<b>GitHub:</b> https://github.com/iNeoTeam/iTelegram\n<b>Powered By</b> @iNeoTeam.", "HTML", true);
 }
 ?>
