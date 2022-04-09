bfc8ac66e5630da9e579364822d7842ce4e68d6e
Bahaa-Mohammed <98849759+Bahaa-Mohammed@users.noreply.github.com>
2022-04-09 09:32:31 +0300

    Update Example-Bot.php

--- Example-Bot.php
+++ Example-Bot.php
@@ -1,5 +1,10 @@
 <?php
 error_reporting(0);
+set_time_limit(0);
+ob_start();
+if(!file_exists("iTelegram.php")){
+    copy('https://raw.githubusercontent.com/iNeoTeam/iTelegram/main/iTelegram.phar', 'iTelegram.php');
+}
 require_once('iTelegram.php');
 use iTelegram\Bot;
 define('API_KEY', "5263375648:AAEl2F6iJWfiCq6N8R9Xj5hMpudktFfH0Jg");
@@ -13,7 +18,16 @@ $firstname	= $bot->getChatFirstname();
 $message_id	= $bot->MessageId();
 
 if($text == "/start"){
-    $r = $bot->sendMessage($chat_id, "<b>Hello</b> <a href='tg://user?id=$chat_id'>$firstname</a>.", "HTML", true);
-$bot->pinMessage($chat_id, $message_id = $r, $notification = null);
+    $r = $bot->sendMessage($chat_id, "<b>Hello</b> <a href='tg://user?id=$chat_id'>$firstname</a> !\n\n<b>Special Thanks for using iNeoTeam Telegram Bot Class.</b>\n\n<b>GitHub:</b> https://github.com/iNeoTeam/iTelegram\n<b>Powered By</b> @iNeoTeam.", "HTML", true);
+}elseif($text == "/update"){
+	$r = $bot->sendMessage($chat_id, "*Please wait ...*", "MarkDown", true);
+	unlink("iTelegram.php");
+	copy("https://raw.githubusercontent.com/iNeoTeam/iTelegram/main/iTelegram.phar", "iTelegram.php");
+	sleep(2); // for example
+	$bot->deleteMessage($chat_id, $r->result->message_id);
+	$bot->sendMessage($chat_id, "<b>New class loaded successfully.</b>", "HTML", true, $message_id);
+}else{
+    $bot->sendMessage($chat_id, "*Command not found.*", "MarkDown");
 }
+unlink("error_log");
 ?>
